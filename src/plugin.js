f50bea8370d6764bf1f9d215898c4aff702fadc8
Laurence Elsdon <laurence.elsdon@matterlab.co>
2021-10-14 16:32:15 +0100

    Simplify the default plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,16 +1,19 @@
-const createBlock = async (w,l,h) => {
-    const pt1 = await WSM.Geom.Point3d(0,0,0);
-    const pt2 = await WSM.Geom.Point3d(w,l,h);
-    const histID = await FormIt.GroupEdit.GetEditingHistoryID();
-    console.log(histID, pt1, pt2)
+document
+    .getElementById("CreateButton")
+    .addEventListener("click", async () => {
+        const inputs = {
+            width: Number(document.getElementById("Width").value),
+            height: Number(document.getElementById("Height").value),
+            length: Number(document.getElementById("Length").value)
+        }
 
-    const test = await WSM.APICreateBlock(histID, pt1, pt2);
-}
+        const point1 = await WSM.Geom.Point3d(0,0,0)
+        const point2 = await WSM.Geom.Point3d(inputs.width, inputs.length, inputs.height)
+        const historyID = await FormIt.GroupEdit.GetEditingHistoryID()
 
-document.getElementById("CreateBlockBtn").addEventListener("click", () => {
-    const w = Number(document.getElementById("Width").value);
-    const h = Number(document.getElementById("Height").value);
-    const l = Number(document.getElementById("Length").value);
+        console.log(historyID, point1, point2)
 
-    createBlock(w,l,h);
-});
+        const cuboid = await WSM.APICreateBlock(historyID, point1, point2)
+
+        console.log(cuboid)
+    })
