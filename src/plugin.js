9f1453173ca3a9dd865eb16db4e91f702bbf6fde
Laurence Elsdon <laurence.elsdon@matterlab.co>
2021-10-14 18:01:07 +0100

    Create a Hyperbolic Paraboloid

--- src/plugin.js
+++ src/plugin.js
@@ -3,17 +3,34 @@ document
     .addEventListener("click", async () => {
         const inputs = {
             width: Number(document.getElementById("Width").value),
+            length: Number(document.getElementById("Length").value),
             height: Number(document.getElementById("Height").value),
-            length: Number(document.getElementById("Length").value)
+            divisions: Number(document.getElementById("Divisions").value)
         }
 
-        const point1 = await WSM.Geom.Point3d(0,0,0)
-        const point2 = await WSM.Geom.Point3d(inputs.width, inputs.length, inputs.height)
+        const constants = {
+            radius: (inputs.width / inputs.divisions) * 0.75,
+            accuracy: 5
+        }
+
+        const start = -0.5;
+        const end = 0.5;
+        const step = 1 / inputs.divisions;
+        const xCoordinates = _.range(start, end, step).concat(end)
+        const yCoordinates = _.range(start, end, step).concat(end)
+
+        console.log(xCoordinates)
+        console.log(yCoordinates)
+
         const historyID = await FormIt.GroupEdit.GetEditingHistoryID()
+        for(const x of xCoordinates) {
+            for(const y of yCoordinates) {
+                const a = 1
+                const b = -1
+                const z = (a * Math.pow(x, 2)) + (b * Math.pow(y, 2))
 
-        console.log(historyID, point1, point2)
-
-        const cuboid = await WSM.APICreateBlock(historyID, point1, point2)
-
-        console.log(cuboid)
+                let point = await WSM.Geom.Point3d(x * inputs.width, y * inputs.length, z * inputs.height)
+                await WSM.APICreateHemisphere(historyID, constants.radius, point, constants.accuracy)
+            }
+        }
     })
