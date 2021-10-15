57733d9f24269963a7349ae9663ef35cc38a1e57
Laurence Elsdon <laurence.elsdon@matterlab.co>
2021-10-15 10:30:59 +0100

    Added docs

--- src/plugin.js
+++ src/plugin.js
@@ -1,50 +1,75 @@
+/**
+ * The current plugin state
+ */
 const state = {
     history: []
 }
 
+/**
+ *  Create button click event handler
+ */
+const onCreateButtonClick = async () => {
 
-// Create button click event handler
-document
-    .getElementById("CreateButton")
-    .addEventListener("click", async () => {
-        const inputs = {
-            width: Number(document.getElementById("Width").value),
-            length: Number(document.getElementById("Length").value),
-            height: Number(document.getElementById("Height").value),
-            divisions: Number(document.getElementById("Divisions").value)
+    // Create an object of all the HTML <input> values
+    const inputs = {
+        width: Number(document.getElementById("Width").value),
+        length: Number(document.getElementById("Length").value),
+        height: Number(document.getElementById("Height").value),
+        divisions: Number(document.getElementById("Divisions").value)
+    }
+
+    // Create an object with some calculated constants
+    const constants = {
+        radius: (inputs.width / inputs.divisions) * 0.75,
+        accuracy: 5
+    }
+
+    // Create a sequence of numbers from -0.5 to 0.5
+    const start = -0.5;
+    const end = 0.5;
+    const step = 1 / inputs.divisions;
+    const xCoordinates = _.range(start, end, step).concat(end)
+    const yCoordinates = _.range(start, end, step).concat(end)
+
+    // Create a new history ID and store it in the plugin state object
+    state.history.push(await FormIt.GroupEdit.GetEditingHistoryID())
+    
+    // Loop through each of the x and y value sequences
+    for(const x of xCoordinates) {
+        for(const y of yCoordinates) {
+            // Determine the x, y, and z values
+            const pointX = x * inputs.width;
+            const pointY = y * inputs.length;
+            const pointZ = (Math.pow(x, 2) - Math.pow(y, 2)) * inputs.height
+
+            // Create a 3D point with theose values
+            let point = await WSM.Geom.Point3d(pointX, pointY, pointZ)
+
+            // Get the latest history ID from the plugin state object
+            const latestHistoryID = _.last(state.history)
+
+            // Create a hemisphere geometry in FormIt
+            WSM.APICreateHemisphere(latestHistoryID, constants.radius, point, constants.accuracy)
         }
+    }
+}
 
-        const constants = {
-            radius: (inputs.width / inputs.divisions) * 0.75,
-            accuracy: 5
-        }
+/**
+ *  Undo button click event handler
+ */
+const onUndoButtonClick = async () => {
+    // Get the latest history ID from the plugin state object
+    const latestHistoryID = _.last(state.history)
 
-        const start = -0.5;
-        const end = 0.5;
-        const step = 1 / inputs.divisions;
-        const xCoordinates = _.range(start, end, step).concat(end)
-        const yCoordinates = _.range(start, end, step).concat(end)
+    // Delete all the geometry created in the last history operation
+    WSM.APIDeleteHistory(latestHistoryID)
 
-        console.log(xCoordinates)
-        console.log(yCoordinates)
+    // Remove the latest history ID from the plugin state object
+    state.history.pop()
+}
 
-        state.history.push(await FormIt.GroupEdit.GetEditingHistoryID())
-        for(const x of xCoordinates) {
-            for(const y of yCoordinates) {
-                const a = 1
-                const b = -1
-                const z = (a * Math.pow(x, 2)) + (b * Math.pow(y, 2))
+// Set the create button click event listener
+document.getElementById("CreateButton").addEventListener("click", onCreateButtonClick)
 
-                let point = await WSM.Geom.Point3d(x * inputs.width, y * inputs.length, z * inputs.height)
-                WSM.APICreateHemisphere(_.last(state.history), constants.radius, point, constants.accuracy)
-            }
-        }
-    })
-
-// Undo button click event handler
-    document
-    .getElementById("UndoButton")
-    .addEventListener("click", async () => {
-        WSM.APIDeleteHistory(_.last(state.history))
-        state.history.pop()
-    })
+// Set the undo button click event listener
+document.getElementById("UndoButton").addEventListener("click", onUndoButtonClick)
