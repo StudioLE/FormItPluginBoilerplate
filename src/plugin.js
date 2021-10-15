46927a1ec81e961ce4f1221372884b8d83931d2d
Laurence Elsdon <laurence.elsdon@matterlab.co>
2021-10-15 10:14:15 +0100

    Added undo event

--- src/plugin.js
+++ src/plugin.js
@@ -1,3 +1,9 @@
+const state = {
+    history: []
+}
+
+
+// Create button click event handler
 document
     .getElementById("CreateButton")
     .addEventListener("click", async () => {
@@ -22,7 +28,7 @@ document
         console.log(xCoordinates)
         console.log(yCoordinates)
 
-        const historyID = await FormIt.GroupEdit.GetEditingHistoryID()
+        state.history.push(await FormIt.GroupEdit.GetEditingHistoryID())
         for(const x of xCoordinates) {
             for(const y of yCoordinates) {
                 const a = 1
@@ -30,7 +36,15 @@ document
                 const z = (a * Math.pow(x, 2)) + (b * Math.pow(y, 2))
 
                 let point = await WSM.Geom.Point3d(x * inputs.width, y * inputs.length, z * inputs.height)
-                WSM.APICreateHemisphere(historyID, constants.radius, point, constants.accuracy)
+                WSM.APICreateHemisphere(_.last(state.history), constants.radius, point, constants.accuracy)
             }
         }
+    })
+
+// Undo button click event handler
+    document
+    .getElementById("UndoButton")
+    .addEventListener("click", async () => {
+        WSM.APIDeleteHistory(_.last(state.history))
+        state.history.pop()
     })
